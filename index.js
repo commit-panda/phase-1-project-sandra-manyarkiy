cff8968c5182e5ba0eab3f8af85ee029de814ec3
commit-panda <sjmanyarkiy@gmail.com>
2025-10-15 16:31:30 +0300

    added catch to catch any errors

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const API_KEY = '582e4a592a05d366493424e26c60ffcb'
 
 // fetch weather information from Open Weather API
-async function fetchWeatherInfo(city, API_KEY) {
+async function fetchWeatherInfo(city = "Nairobi", API_KEY) {
     try{
         const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
         const data = res.json();
 
-        console.log(data)
 
         const cloud_name = data.weather[0].main
         const cloud_description = data.weather[0].description
@@ -15,9 +14,19 @@ async function fetchWeatherInfo(city, API_KEY) {
         const humidity = data.main.humidity
         const wind_speed = data.wind.speed
 
+       return `The weather in ${city} is: 
+       <ul>
+       <li>Clouds: ${cloud_name}</li>
+       <li>Cloud Description: ${cloud_description}</li>
+       <li>Clouds: ${temp}</li>
+       <li>Feels like: ${feels_like}</li>
+       <li>Humidity: ${humidity}</li>
+       <li>Wind Speed: ${wind_speed}</li>
+       <ul>`
     }
-    catch {
-
+    catch(err){
+        console.error("ERROR:", err)
+        return "Could not fetch weather information. Please try again." 
     }
     
 }
